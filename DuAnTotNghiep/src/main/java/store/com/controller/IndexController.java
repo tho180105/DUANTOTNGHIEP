f66797f19b966c9e5c71258a4d5ead5c1f6b842c
LeNgocAnh11 <87508681+LeNgocAnh11@users.noreply.github.com>
2022-10-01 08:36:20 +0700

    Anh
    a

--- DuAnTotNghiep/src/main/java/store/com/controller/IndexController.java
+++ DuAnTotNghiep/src/main/java/store/com/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 	
 	 
-	@RequestMapping("home")
+	@RequestMapping("/home")
 	public String home() {
 		return "home/home";
 	}
