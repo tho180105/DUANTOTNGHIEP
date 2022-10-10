f3fdc2d99daf69274f2e2939876cebfc4edface1
khang <trannguyenkhang273@gmail.com>
2022-10-10 22:00:16 +0700

    khang chuyen nhanh bi loi index, tho lam chung

--- DuAnTotNghiep/src/main/java/store/com/controller/LoginController.java
+++ DuAnTotNghiep/src/main/java/store/com/controller/LoginController.java
@@ -31,7 +31,7 @@ public class LoginController {
 	}
 	
 	@RequestMapping("/security/login/success")
-	public String success(Model model,Authentication auth ){
+	public String success(Model model,Authentication auth){
 		if(cookie.get("remember-me")!=null) {
 			Cookie acc = cookie.get("JSESSIONID");
 			cookie.add("JSESSIONID", acc.getValue(), 24*30);
